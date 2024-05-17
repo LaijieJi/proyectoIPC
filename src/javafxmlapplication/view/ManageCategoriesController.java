c09c7fab5091e68ab3c6d256b20ed8eca0dfcc24
icatfor <icatalafort@gmail.com>
2024-05-17 12:14:50 +0200

    added things
    added modify category window and some of its implementation

--- src/javafxmlapplication/view/ManageCategoriesController.java
+++ src/javafxmlapplication/view/ManageCategoriesController.java
@@ -4,14 +4,22 @@
  */
 package javafxmlapplication.view;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -56,11 +64,30 @@ public class ManageCategoriesController implements Initializable {
     }
 
     @FXML
-    private void onAddButtonPressed(ActionEvent event) {
+    private void onAddButtonPressed(ActionEvent event) throws IOException{
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/ModifyCategory.fxml"));
+        AnchorPane root = (AnchorPane) myLoader.load();
+        
+        Scene scene = new Scene(root,400,400);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Add category");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
     }
 
     @FXML
     private void onDeleteCategoryButtonPressed(ActionEvent event) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Delete category");
+        alert.setContentText("Are you sure you want to delete this category?");
+        
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
     }
     
 }
---
+++ src/javafxmlapplication/view/ModifyCategoryController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author ICATFOR
+ */
+public class ModifyCategoryController implements Initializable {
+
+    @FXML
+    private TextField nameText;
+    @FXML
+    private TextField descriptionText;
+    @FXML
+    private ColorPicker colorPicker;
+    @FXML
+    private Button acceptButton;
+    @FXML
+    private Button cancelButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void acceptAction(ActionEvent event) {
+        nameText.getScene().getWindow().hide();
+    }
+
+    @FXML
+    private void cancelAction(ActionEvent event) {
+        nameText.getScene().getWindow().hide();
+    }
+    
+}
